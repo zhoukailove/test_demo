604755dde883f376805bb7ab992c06f2ce70ba30
zhoukai <fayake314@gmail.com>
2015-12-15 15:42:32 +0800

    two push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestDemo::Application.config.secret_key_base = '0844ee982e8dd40714325c921017fa37fe0eb03f88555f176339cc946ef77ce02f06673b71971e9387487618d5e505a463758554d442b1382f98962ac3e4d497'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestDemo::Application.config.session_store :cookie_store, key: '_test_demo_session'
